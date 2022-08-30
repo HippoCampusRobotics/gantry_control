c0f391cabb01a62e30785bd4b272db77fd1ff4b4
Thies Lennart Alff <lennart.alff@gmail.com>
2022-08-30 16:00:43 +0200

    fixed unresponsive x-axis motor

--- src/gantry_control/__init__.py
+++ src/gantry_control/__init__.py
@@ -6,9 +6,13 @@ from . import motor
 def create(port, baud, timeout) -> motor.BaseMotor:
     m = motor.BaseMotor(port=port, baud=baud, timeout=timeout)
     t = m.get_type()
-    if t == "CS":
+    if t == 'CS':
         return mcbl2805.Motor(port=port, baud=baud, timeout=timeout)
     elif t == 'CS-BX4':
         return faulhaber_rs232.Motor(port=port, baud=baud, timeout=timeout)
+    elif t == '':
+        raise IOError(
+            'Motor not responding.\nIs it switched on?\nAre all '
+            'wires connected?\nHave you tried turning it off and on again?')
     else:
         raise RuntimeError(f'Unknown motor type: {t}')
